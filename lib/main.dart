165dee623ee16c01b34352f4fb6d51b6ac5aeb68
bartoz1 <nil112101@gmail.com>
2022-11-27 14:07:10 +0100

    BB-15 added languages support

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:bike_buddy/pages/ride_page.dart';
 import 'package:bike_buddy/pages/settings_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'l10n/l10n.dart';
 import 'pages/main_page.dart';
 
 void main() {
@@ -22,6 +25,12 @@ class BikeBuddy extends StatelessWidget {
         '/ride': (context) => RidePage(),
         '/settings': (context) => SettingsPage(),
       },
+      supportedLocales: L10n.all,
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
     );
   }
 }
