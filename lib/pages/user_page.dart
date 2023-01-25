3cdd087c9b2d2ae92e686688cc17c51d46413689
jan-Kulpas <krzysztof.a.kulpinski@gmail.com>
2023-01-25 17:26:07 +0100

    BB-39: Resize and Translate User Page

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:bike_buddy/components/bb_appbar.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class UserPage extends StatefulWidget {
   const UserPage({super.key});
@@ -12,18 +13,26 @@ class UserPage extends StatefulWidget {
 }
 
 class _UserPageState extends State<UserPage> {
+  //temp variables to be replace with data from a provider
+  final username = "Username";
+  final distance = 420;
+  final calories = 7;
+  final time = const Duration(hours: 2, minutes: 10, seconds: 37);
+  final streak = 9;
+  final maxDistance = 200;
+
   bool showWeekly = false;
 
   @override
   Widget build(BuildContext context) {
+    final localization = AppLocalizations.of(context)!;
     return Scaffold(
       appBar: const BBAppBar(),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          SizedBox(
-            width: double.infinity,
-            height: 220,
+          Expanded(
+            flex: 3,
             child: Stack(
               children: [
                 FractionallySizedBox(
@@ -54,82 +63,87 @@ class _UserPageState extends State<UserPage> {
               ],
             ),
           ),
-          const Center(
+          Center(
             child: Padding(
-              padding: EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
               child: Text(
-                "Username",
-                style: TextStyle(fontSize: 18),
+                username,
+                style: const TextStyle(fontSize: 18),
               ),
             ),
           ),
-          const XPBar(),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.85,
-            height: MediaQuery.of(context).size.height * 0.45,
-            margin: const EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              color: Colors.grey.shade400,
-              border: Border.all(color: Colors.grey.shade700, width: 5),
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: LayoutBuilder(
-              builder: (context, constraints) => Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      SizedBox(
-                        width: constraints.maxWidth * 0.8,
-                        child: const Text(
-                          "Statystyki",
-                          textAlign: TextAlign.center,
+          const Expanded(
+            flex: 1,
+            child: XPBar(),
+          ),
+          Expanded(
+            flex: 6,
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.85,
+              margin: const EdgeInsets.only(bottom: 30),
+              decoration: BoxDecoration(
+                color: Colors.grey.shade400,
+                border: Border.all(color: Colors.grey.shade700, width: 5),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: LayoutBuilder(
+                builder: (context, constraints) => Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          width: constraints.maxWidth * 0.8,
+                          child: Text(
+                            localization.u_statistics,
+                            textAlign: TextAlign.center,
+                          ),
                         ),
-                      ),
-                      Switch(
-                        activeColor: Colors.deepPurple,
-                        activeTrackColor: Colors.deepPurple.shade300,
-                        inactiveThumbColor: Colors.blueGrey.shade600,
-                        inactiveTrackColor: Colors.grey.shade500,
-                        value: showWeekly,
-                        onChanged: (value) => setState(() {
-                          showWeekly = value;
-                        }),
-                      ),
-                    ],
-                  ),
-                  Expanded(
-                    child: ListView(
-                      children: const [
-                        StatsTile(
-                          icon: Icon(Icons.pedal_bike),
-                          title: "Przejechany Dystans",
-                          value: "420km",
-                        ),
-                        StatsTile(
-                          icon: Icon(Icons.local_fire_department),
-                          title: "Spalone kalorie",
-                          value: "7kcal",
-                        ),
-                        StatsTile(
-                          icon: Icon(Icons.timer),
-                          title: "Czas jazdy",
-                          value: "02:10:37",
-                        ),
-                        StatsTile(
-                          icon: Icon(Icons.calendar_month),
-                          title: "Dni pod rząd",
-                          value: "69",
-                        ),
-                        StatsTile(
-                          icon: Icon(Icons.emoji_events),
-                          title: "Najdłuższy przejazd",
-                          value: "200km",
+                        Switch(
+                          activeColor: Colors.deepPurple,
+                          activeTrackColor: Colors.deepPurple.shade300,
+                          inactiveThumbColor: Colors.blueGrey.shade600,
+                          inactiveTrackColor: Colors.grey.shade500,
+                          value: showWeekly,
+                          onChanged: (value) => setState(() {
+                            showWeekly = value;
+                          }),
                         ),
                       ],
                     ),
-                  ),
-                ],
+                    Expanded(
+                      child: ListView(
+                        children: [
+                          StatsTile(
+                            icon: const Icon(Icons.pedal_bike),
+                            title: localization.u_distance,
+                            value: "${distance}km",
+                          ),
+                          StatsTile(
+                            icon: const Icon(Icons.local_fire_department),
+                            title: localization.u_calories,
+                            value: "${calories}kcal",
+                          ),
+                          StatsTile(
+                            icon: const Icon(Icons.timer),
+                            title: localization.u_time,
+                            value: time.toString().split('.').first.padLeft(8, "0"),
+                          ),
+                          StatsTile(
+                            icon: const Icon(Icons.calendar_month),
+                            title: localization.u_streak,
+                            value: streak.toString(),
+                          ),
+                          StatsTile(
+                            icon: const Icon(Icons.emoji_events),
+                            title: localization.u_max_distance,
+                            value: "${maxDistance}km",
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -140,11 +154,7 @@ class _UserPageState extends State<UserPage> {
 }
 
 class StatsTile extends StatelessWidget {
-  const StatsTile(
-      {required this.icon,
-      required this.title,
-      required this.value,
-      super.key});
+  const StatsTile({required this.icon, required this.title, required this.value, super.key});
 
   final Icon icon;
   final String title;
