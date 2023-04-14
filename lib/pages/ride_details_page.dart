e5cae3009dd117ce742d531ae328cb7cb337b8c2
Noxxxxious <michal.wrzosek.robert@gmail.com>
2023-04-14 19:16:04 +0200

    translations, textStyles const variables, map placeholder added

--- lib/pages/ride_details_page.dart
+++ lib/pages/ride_details_page.dart
@@ -7,7 +7,6 @@ class RideDetailsPage extends StatefulWidget {
   const RideDetailsPage({super.key});
 
   static const String routeName = '/ride-details';
-
   @override
   State<RideDetailsPage> createState() => _RideDetailsPageState();
 }
@@ -15,134 +14,97 @@ class RideDetailsPage extends StatefulWidget {
 class _RideDetailsPageState extends State<RideDetailsPage> {
   @override
   Widget build(BuildContext context) {
+    const double TextSize = 16.0;
+    const DetailKeyTextStyle = TextStyle(
+      fontSize: TextSize,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    );
+    const DetailValTextStyle = TextStyle(fontSize: TextSize);
     final translations = AppLocalizations.of(context)!;
     return Scaffold(
       appBar: const BBAppBar(),
       body: ListView(
         children: [
-          Center(
-            child: Container(
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              child: Text(
-                translations.h_ride_details,
-                style: const TextStyle(fontSize: 30),
-              ),
-            ),
-          ),
           Card(
-            elevation: 4.0,
-            margin: const EdgeInsets.all(16.0),
+            margin: const EdgeInsets.all(TextSize),
             child: Padding(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(TextSize),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  const Text(
-                    'Ride Details',
-                    style:
-                        TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 16.0),
+                  Text(translations.h_ride_details, style: const TextStyle(fontSize: 22.0)),
+                  const SizedBox(height: TextSize),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
                       Expanded(
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const <Widget>[
-                            Text(
-                              'Total Distance',
-                              style: TextStyle(
-                                  fontSize: 16.0, fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(height: 8.0),
-                            Text(
-                              '10 km', // Example text
-                              style: TextStyle(fontSize: 16.0),
-                            ),
+                          children: <Widget>[
+                            Text(translations.h_ride_details_total_distance, style: DetailKeyTextStyle,),
+                            const SizedBox(height: TextSize / 2),
+                            const Text('100 km', style: DetailValTextStyle,),
                           ],
                         ),
                       ),
                       Expanded(
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const <Widget>[
-                            Text(
-                              'Ride Duration',
-                              style: TextStyle(
-                                  fontSize: 16.0, fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(height: 8.0),
-                            Text(
-                              '45 minutes', // Example text
-                              style: TextStyle(fontSize: 16.0),
-                            ),
+                          children: <Widget>[
+                            Text(translations.h_ride_details_ride_duration, style: DetailKeyTextStyle,),
+                            const SizedBox(height: TextSize / 2),
+                            const Text('45 minutes', style: DetailValTextStyle,),
                           ],
                         ),
                       ),
                     ],
                   ),
-                  const SizedBox(height: 16.0),
+                  const SizedBox(height: TextSize),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
                       Expanded(
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const <Widget>[
-                            Text(
-                              'Max Speed',
-                              style: TextStyle(
-                                  fontSize: 16.0, fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(height: 8.0),
-                            Text(
-                              '30 km/h', // Example text
-                              style: TextStyle(fontSize: 16.0),
-                            ),
+                          children: <Widget>[
+                            Text(translations.h_ride_details_maximum_speed, style: DetailKeyTextStyle,),
+                            const SizedBox(height: TextSize / 2),
+                            const Text('30 km/h', style: DetailValTextStyle,),
                           ],
                         ),
                       ),
                       Expanded(
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const <Widget>[
-                            Text(
-                              'Average Speed',
-                              style: TextStyle(
-                                  fontSize: 16.0, fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(height: 8.0),
-                            Text(
-                              '15 km/h', // Example text
-                              style: TextStyle(fontSize: 16.0),
-                            ),
+                          children: <Widget>[
+                            Text(translations.h_ride_details_average_speed, style: DetailKeyTextStyle,),
+                            const SizedBox(height: TextSize / 2),
+                            const Text('15 km/h', style: DetailValTextStyle,),
                           ],
                         ),
                       ),
                     ],
                   ),
+                  const SizedBox(height: TextSize),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
                       Expanded(
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const <Widget>[
-                            Text(
-                              'Calories Burnt',
-                              style: TextStyle(
-                                  fontSize: 16.0, fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(height: 8.0),
-                            Text(
-                              '500 kcal', // Example text
-                              style: TextStyle(fontSize: 16.0),
-                            ),
+                          children: <Widget>[
+                            Text(translations.h_ride_details_calories_burnt, style: DetailKeyTextStyle,),
+                            const SizedBox(height: TextSize / 2),
+                            const Text('500 kcal', style: DetailValTextStyle,),
                           ],
                         ),
                       ),
                     ],
+                  ),
+                  const SizedBox(height: TextSize * 2),
+                  Center(
+                      child: Image.asset("images/map1.png", fit: BoxFit.cover)
                   ),
                 ],
               ),
